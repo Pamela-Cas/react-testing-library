191c8d9a66b52226790fb08b22d254b6bf5fb826
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-02-21 15:57:28 -0300

    primeiro requesito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+describe(
+  'Teste se o topo da aplicação contém um conjunto fixo de links de navegação',
+  () => {
+    beforeEach(() => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+    });
+
+    it('O primeiro link deve possuir o texto Home.', () => {
+      const home = screen.getByRole('link', { name: /home/i });
+      expect(home).toBeInTheDocument();
+    });
+
+    it('O segundo link deve possuir o texto About.', () => {
+      const about = screen.getByRole('link', { name: /about/i });
+      expect(about).toBeInTheDocument();
+    });
+
+    it('O terceiro link deve possuir o texto Favorite Pokémons.', () => {
+      const favoritePokemons = screen.getByRole('link', { name: /favorite pokémon/i });
+      expect(favoritePokemons).toBeInTheDocument();
+    });
+  },
+);
