0139338e222e1220c89a2b48fb762e97f389d72a
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-02-23 15:15:40 -0300

    requesito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,30 +1,40 @@
+// Realizando o import
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import renderWithRouter from '../functionRenderWithRouter/renderWithRouter';
-import About from '../components/About';
+import App from '../App';
 
+// renderizando o about
 describe('Teste o componente <About.js />', () => {
-  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
-    const { getByRole } = renderWithRouter(<About />);
-    const title = getByRole('heading', { name: /About Pokédex/i, level: 2 });
-    expect(title).toBeInTheDocument();
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
   });
-  // Teste se a página contém dois parágrafos com texto sobre a Pokédex.
-  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-    const p1 = 'This application simulates a Pokédex,'
-    + ' a digital encyclopedia containing all Pokémons';
 
-    const p2 = 'One can filter Pokémons by type, and see'
-    + ' more details for each one of them';
-    const { getByText } = renderWithRouter(<About />);
-    const paragraph1 = getByText(p1);
-    const paragraph2 = getByText(p2);
+  // teste h2 com about Pokedex
+  it('Contém um heading h2 com o texto About Pokédex', () => {
+    const headingPokedex = screen.getByRole(
+      'heading', { level: 2, name: /About Pokédex/i },
+    );
+    expect(headingPokedex).toBeInTheDocument();
+  });
+
+  it('Contém dois parágrafos com texto sobre a Pokédex', () => {
+    const paragraph1 = screen.getByText(/This application simulates a Pokédex/i);
+    const paragraph2 = screen.getByText(/One can filter Pokémons by type/i);
+
     expect(paragraph1 && paragraph2).toBeInTheDocument();
   });
 
-  // Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png
-  it('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
-    const { getByRole } = renderWithRouter(<About />);
-    const image = getByRole('img', { name: /Pokédex/i });
-    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  // test imagem pokedex
+  it('Contém a imagem de uma Pokédex', () => {
+    const imgPokedex = screen.getByAltText(/pokédex/i);
+    expect(imgPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
